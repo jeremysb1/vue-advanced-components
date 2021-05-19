39296a3ec22e80d28186cc394b6daa3fbb08af78
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-05-19 16:20:08 -0400

    inserting content w/slot

--- slots/src/App.vue
+++ slots/src/App.vue
@@ -1,5 +1,17 @@
 <template>
-  <app-form />
+  <app-form>
+    <div class="help">
+      <p>This is some help text.</p>
+    </div>
+    <div class="fields">
+      <input type="text" placeholder="email">
+      <input type="text" placeholder="username">
+      <input type="password" placeholder="password">
+    </div>
+    <div class="buttons">
+      <button type="submit">Submit</button>
+    </div>
+  </app-form>
 </template>
 
 <script>
--- slots/src/components/Form.vue
+++ slots/src/components/Form.vue
@@ -1,15 +1,5 @@
 <template>
   <form>
-    <div class="help">
-      <p>This is some help text.</p>
-    </div>
-    <div class="fields">
-      <input type="text" placeholder="email">
-      <input type="text" placeholder="username">
-      <input type="password" placeholder="password">
-    </div>
-    <div class="buttons">
-      <button type="submit">Submit</button>
-    </div>
+    <slot></slot>
   </form>
 </template>
