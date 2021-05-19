92740b672c780bffec1450185aa9d8dd1be73a3c
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-05-19 16:10:46 -0400

    creating slots

---
+++ slots/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
